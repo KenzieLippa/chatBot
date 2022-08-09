33b0832db6dde63382db27f72e162a6efee93681
KenzieLippa <mckenzie.lippa1@marist.edu>
2022-08-09 10:44:39 -0400

    The files transfered over

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, session
+from twilio.twiml.messaging_response import MessagingResponse
+from testBot import ask, append_interaction_to_chat_log
+
+app = Flask(__name__)
+#web hook
+#if bots being dumb change the key
+app.config['SECRET_KEY'] = '89djhff9lhdk93'
+@app.route('/testBot', methods = ['POST'])
+
+#including information
+def test():
+    incoming_msg = request.values['Body']
+    chat_log = session.get('chat_log')
+    answer = ask(incoming_msg, chat_log)
+    #append and update the chat log
+    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer, chat_log)
+
+    msg = MessagingResponse()
+    msg.message(answer)
+    #routing back to api
+    return str(msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
